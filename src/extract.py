cbc1a0b17071ae54b93de604fa2839e1a7cc80e2
Alexey Akimov <alexvakimov@gmail.com>
2016-02-18 15:36:15 -0500

    Notably revised workflow and smaller script. The only thing is that the couplings are zero in my case - I must have overlooked something little, but the organization of computations is much simplier now - everything is done from the /run directory - that is where the results (in /res subdirectory) will be printed out, that is where all input data and files (including temporary) are located, that is where the submit script is located and where from it is submitted. No data and results should be in the /src directory - it is purely for the code

--- src/extract.py
+++ src/extract.py
@@ -27,7 +27,7 @@ import math
 
 # First, we add the location of the library to test to the PYTHON path
 cwd = "/projects/academic/alexeyak/alexeyak/libra-dev/libracode-code"
-print "Using the Libra installation at", cwd
+#print "Using the Libra installation at", cwd
 sys.path.insert(1,cwd+"/_build/src/mmath")
 sys.path.insert(1,cwd+"/_build/src/qchem")
 from libmmath import *
--- src/nve.py
+++ src/nve.py
@@ -16,8 +16,8 @@
 
 
 from create_gamess_input import *
-from create_pdb_file import *
-from exe_gamess import *
+#from create_pdb_file import *
+#from exe_gamess import *
 from gamess_to_libra import *
 
 import os
@@ -25,12 +25,12 @@ import sys
 import math
 
 # Fisrt, we add the location of the library to test to the PYTHON path
-cwd= "/projects/academic/alexeyak/kosukesa/libracode-code/"
+cwd = "/projects/academic/alexeyak/alexeyak/libra-dev/libracode-code"
 print "Current working directory", cwd
-sys.path.insert(1,cwd+"_build/src/mmath")
-sys.path.insert(1,cwd+"_build/src/chemobjects")
-sys.path.insert(1,cwd+"_build/src/hamiltonian")
-sys.path.insert(1,cwd+"_build/src/dyn")
+sys.path.insert(1,cwd+"/_build/src/mmath")
+sys.path.insert(1,cwd+"/_build/src/chemobjects")
+sys.path.insert(1,cwd+"/_build/src/hamiltonian")
+sys.path.insert(1,cwd+"/_build/src/dyn")
 #sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/hamiltonian_atomistic")
 
 print "\nTest 1: Importing the library and its content"
@@ -39,153 +39,141 @@ from libchemobjects import *
 from libhamiltonian import *
 from libdyn import *
 #from cyghamiltonian_atomistic import *
-
 from LoadPT import * # Load_PT
-from LoadMolecule import * # Load_Molecule
 
 ##############################################################
 
-def construct_systems(Grad):
+
+def exe_gamess(params):
+    ##
+    # This is a function that call GAMESS execution on the compute node
+    # \param[in] params Input data containing all manual settings and some extracted data.
+    #
+
+    inp = params["gms_inp"]
+    out = params["gms_out"]
+    nproc = params["nproc"]
+    os.system("/usr/bin/time rungms.slurm %s 01 %s > %s" % (inp,nproc,out))
+
+    os.system("rm *.dat")
+
+
+
+def run_MD(syst,ao,E,C,data,params):
     ##
     # Finds the keywords and their patterns and extracts the parameters
-    # \param[in] Grad : the forces extracted from gamess output file.
-    # This function returns SYSTEM and NUCLEAR constructor 
-    # which will be used in classical MD.
+    # \param[in] syst   System object that includes atomic system information.
+    # \param[in] ao     Atomic orbital basis
+    # \param[in] E      Molecular orbital energies
+    # \param[in] C      MO-LCAO coefficients
+    # \param[in] data   Data extracted from GAMESS output file, in the dictionary form.
+    # \param[in] params Input data containing all manual settings and some extracted data.
+    # This function executes classical MD in Libra and electronic structure calculation
+    # in GAMESS iteratively.
     #
-    # Used in:  main.py/main/nve/nve_settings
-
-    # Create Universe and populate it
-    U = Universe()
-    verbose = 0
-    Load_PT(U, "elements.dat", verbose)
+    # Used in:  main.py/main/nve
 
 
-    verb = 0
-    assign_rings = 0
+    # Open and close energy and trajectory files - this will effectively 
+    # make them empty (to remove older info, in case we restart calculations)
+    fe = open(params["ene_file"],"w")
+    fe.close()
 
-    syst = System()
-    Load_Molecule(U, syst, os.getcwd()+"/temp.pdb", "pdb_1")
+    ft = open(params["traj_file"],"w")
+    ft.close()
+    
+    dt = params["dt_nucl"]
+    Nsnaps = params["Nsnaps"]
+    Nsteps = params["Nsteps"]
 
 
-    syst.show_atoms()
-    #syst.show_fragments()
-    #syst.show_molecules()
-    print "Number of atoms in the system = ", syst.Number_of_atoms
-    #atlst1 = range(1,syst.Number_of_atoms+1)
-
-    #--------------------- Molecular dynamics ----------------------
-    # Nuclear DOFs
+    # Create a variable that will contain propagated nuclear DOFs
     mol = Nuclear(3*syst.Number_of_atoms)
     syst.extract_atomic_q(mol.q)
     syst.extract_atomic_p(mol.p)
+    syst.extract_atomic_f(mol.f)
     syst.extract_atomic_mass(mol.mass)
 
-    Natoms = len(Grad)
-
-    for i in range(0,Natoms):
-        mol.p[3*i], mol.p[3*i+1], mol.p[3*i+2] = 0.0, 0.0, 0.0
-        mol.f[3*i], mol.f[3*i+1], mol.f[3*i+2] = -Grad[i][0], -Grad[i][1], -Grad[i][2]
-
-    for i in range(0,Natoms):
+    # Debug printing
+    for i in xrange(syst.Number_of_atoms):
         print "mass m=",mol.mass[3*i], mol.mass[3*i+1], mol.mass[3*i+2]
         print "coordinates q = ", mol.q[3*i], mol.q[3*i+1], mol.q[3*i+2]
         print "momenta p= ", mol.p[3*i], mol.p[3*i+1], mol.p[3*i+2]
         print "forces f= ",  mol.f[3*i], mol.f[3*i+1], mol.f[3*i+2]
         print "********************************************************"
 
-    return syst, mol
 
-def nve_MD(syst,mol,ao1,E1,C1,data,params):
-    ##
-    # Finds the keywords and their patterns and extracts the parameters
-    # \param[in] syst   : SYSTEM constructor including atomic system information.
-    # \param[in] mol    : Nuclear constructor including atomic positions, momenta, forces etc...
-    # \param[in] ao1    : atomic orbital basis at "t" old
-    # \param[in] E1     : molecular energies at "t" old
-    # \param[in] C1     : molecular coefficients at "t" old
-    # \param[in] data   : data extracted from GAMESS output file, in the dictionary form.
-    # \param[in] params : input data from a submit file, in the dictionary form.
-    # This function executes classical MD in Libra and electronic structure calculation
-    # in GAMESS iteratively.
-    #
-    # Used in:  main.py/main/nve
-
-    f = open("../out/_en_traj.txt","w")
-    dt = params["dt_nuc"]
-    job_tmp = params["GMS_JOB"] + "tmp"
-    Nrounds = params["Nrounds"]
-    Ncycles = params["Ncycles"]
-    Natoms = syst.Number_of_atoms
-    trajfile = "../out/_mol_traj.xyz"
-    os.system("rm -r %s"%(trajfile))
-
-    for i in range(0,Nrounds):
+    # Run actual calculations
+    for i in xrange(Nsnaps):
 
         syst.set_atomic_q(mol.q)
-        syst.print_xyz(trajfile,i)
+        syst.print_xyz(params["traj_file"],i)
 
-        for j in xrange(0,Ncycles):
+        for j in xrange(Nsteps):
 
-            ij = i * Ncycles + j
+            ij = i*Nsteps + j
             mol.propagate_p(0.5*dt)
-            mol.propagate_q(dt)  #---> generate GAMESS input
+            mol.propagate_q(dt) 
+          
+            # ======= Compute forces and energies using GAMESS ============
+            write_gms_inp(data, params, mol)
+            exe_gamess(params)         
 
-            for k in range(0,Natoms):
-                print k,"th atom"
-                print "coordinates q = ", mol.q[3*k], mol.q[3*k+1], mol.q[3*k+2]
-                print "momenta p= ", mol.p[3*k], mol.p[3*k+1], mol.p[3*k+2]
-                print "forces f= ",  mol.f[3*k], mol.f[3*k+1], mol.f[3*k+2]
-                print "********************************************************"
+#            ao, E, C, Grad, data = unpack_file(params["gms_out"])            
+            Grad, data = gamess_to_libra(params, ao, E, C, ij) # this will update AO and gradients
 
-            # GAMESS iteration part
-            write_GMS_INP(data["l_atoms"],data["l_charges"],params["l_gam_for"],mol,job_tmp ,params["GMS_DIR"])
-            exe_gamess(params,job_tmp)                                              # call GAMESS with input file
-            ao1, E1, C1, Grad, data = gamess_to_libra(params, ao1, E1, C1,job_tmp,ij)  # extract info from GAMESS
-            epot = data["tot_ene"]                                                  # total energy from GAMESS
-
-            for k in range(0,len(Grad)):
-                mol.f[3*k], mol.f[3*k+1], mol.f[3*k+2] = -Grad[k][0], -Grad[k][1], -Grad[k][2]  # forces from GAMESS
+            epot = data["tot_ene"]         # total energy from GAMESS
+            for k in xrange(syst.Number_of_atoms):
+                mol.f[3*k]   = -Grad[k][0]
+                mol.f[3*k+1] = -Grad[k][1]
+                mol.f[3*k+2] = -Grad[k][2]
 
             mol.propagate_p(0.5*dt)
 
             ekin = compute_kinetic_energy(mol)
+       
+            t = dt*ij # simulation time in a.u.
+            fe = open(params["ene_file"],"a")
+            fe.write("i= %3i ekin= %8.5f  epot= %8.5f  etot= %8.5f\n" % (i, ekin, epot, ekin+epot)) 
+            fe.close()
 
-        f.write("i= %3i ekin= %8.5f  epot= %8.5f  etot= %8.5f\n" % (i, ekin, epot, ekin+epot))
-        
-    f.close()
 
-def nve_setting(l_atoms,coor_atoms,Grad):
+def init_system(data, g):
     ##
     # Finds the keywords and their patterns and extracts the parameters
-    # \param[in] l_atoms     : a list of atoms.
-    # \param[in] coor_atoms  : a list of atomic positions.
-    # \param[in] Grad        : a list of foeces.
-    # This function returns SYSTEM and NUCLEAR constructors which will be used in
-    # classical MD.
+    # \param[in] data   The list of variables, containing atomic element names and coordinates
+    # \param[in] g      The list of gradients on all atoms
+    # This function returns System object which will be used in classical MD.
     #
     # Used in:  main.py/main/nve
 
-    create_pdb_file(l_atoms,coor_atoms)
 
-    syst, mol = construct_systems(Grad)
+    # Create Universe and populate it
+    U = Universe();   Load_PT(U, "elements.txt", 0)
 
-    return syst, mol
+    syst = System()
 
-def nve(data,params,ao1,E1,C1,Grad):
-    ##
-    # Finds the keywords and their patterns and extracts the parameters
-    # \param[in] data   : data extracted from GAMESS output file, in the dictionary form.
-    # \param[in] params : input data from a submit file, in the dictionary form.
-    # \param[in] ao1    : atomic orbital basis at "t" old
-    # \param[in] E1     : molecular energies at "t" old
-    # \param[in] C1     : molecular coefficients at "t" old
-    # \param[in] Grad   : a list of foeces.
-    # This function executes classical MD in Libra and electronic structure calculation
-    # in GAMESS iteratively ; it outputs the files for simulation of excited electron dynamics.
-    #
-    # Used in:  main.py/main/
+    sz = len(data["coor_atoms"])
+    for i in xrange(sz):
+        atom_dict = {} 
+        atom_dict["Atom_element"] = data["l_atoms"][i]
+
+        # warning: below we take coordinates in Angstroms, no need for conversion here - it will be
+        # done inside
+        atom_dict["Atom_cm_x"] = data["coor_atoms"][i][0]
+        atom_dict["Atom_cm_y"] = data["coor_atoms"][i][1]
+        atom_dict["Atom_cm_z"] = data["coor_atoms"][i][2]
+
+        print "CREATE_ATOM ",atom_dict["Atom_element"]
+        at = Atom(U, atom_dict)
+        at.Atom_RB.rb_force = VECTOR(-g[i][0], -g[i][1], -g[i][2])
+
+        syst.CREATE_ATOM(at)
+
+    syst.show_atoms()
+    print "Number of atoms in the system = ", syst.Number_of_atoms
+
+    return syst
 
 
-    syst, mol = nve_setting(data["l_atoms"],data["coor_atoms"],Grad)
 
-    nve_MD(syst,mol,ao1,E1,C1,data,params)
