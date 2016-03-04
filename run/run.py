6415a3808e974a9d321bdcca583c83d79e17804b
Alexey Akimov <alexvakimov@gmail.com>
2016-03-04 16:16:20 -0500

    Added new default parameters. To the input dictionary

--- run/run.py
+++ run/run.py
@@ -7,13 +7,12 @@ import sys
 import math
 
 # For Kosuke
-libra_bin_path = "/projects/academic/alexeyak/kosukesa/libracode-code/_build/src"
-libra_gamess_int_path = "/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/src"
-
+#libra_bin_path = "/projects/academic/alexeyak/kosukesa/libracode-code/_build/src"
+#libra_gamess_int_path = "/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/src"
 
 # For Alexey
-#libra_bin_path = "/projects/academic/alexeyak/alexeyak/libra-dev/libracode-code/_build/src" # set the path name to the source files in libracode
-#libra_gamess_int_path = "/user/alexeyak/Programming/libra-gamess_interface/src"
+libra_bin_path = "/projects/academic/alexeyak/alexeyak/libra-dev/libracode-code/_build/src" # set the path name to the source files in libracode
+libra_gamess_int_path = "/user/alexeyak/Programming/libra-gamess_interface/src"
 
 os.environ["src_path"] = libra_gamess_int_path
 sys.path.insert(1,os.environ["src_path"]) # Path to the source code
@@ -29,25 +28,29 @@ params["nproc"] = 1            # the number of processors
 params["basis_option"] = 2 # ab initio or Semi-Empirical calculation?  Options: \"ab_initio\" = 1 , \"semi_empirical\" = 2
 params["dt_nucl"] = 20.0  # time step for nuclear dynamics  ex) 20 a.u. = 0.5 fsec
 params["el_mts"] = 1  # electronic time steps per one nuclear time step
-params["Nsnaps"] = 5  # the number of MD rounds
+params["Nsnaps"] = 10 # the number of MD rounds
 params["Nsteps"] = 1  # the number of MD steps per snap
 
 # For Kosuke
-params["res"] = "/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/run/res/" # directory where the energies and trajectories files will be printed out
-params["mo_ham"] = "/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/run/mo_ham/" # directory where MO basis vibronic hamiltonians will be printed out  
-params["sd_ham"] = "/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/run/sd_ham/" # directory where SD basis vibronic hamiltonians will be printed out
+#params["res"] = "/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/run/res/" # directory where the energies and trajectories files will be printed out
+#params["mo_ham"] = "/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/run/mo_ham/" # directory where MO basis vibronic hamiltonians will be printed out  
+#params["sd_ham"] = "/projects/academic/alexeyak/kosukesa/dev/libra-gamess_interface/run/sd_ham/" # directory where SD basis vibronic hamiltonians will be printed out
 
 # For Alexey
-#params["res"] = "/user/alexeyak/Programming/libra-gamess_interface/run/res/"
-#params["mo_ham"] = "/user/alexeyak/Programming/libra-gamess_interface/run/mo_ham/" # directory where MO basis vibronic hamiltonians will be printed out
-#params["sd_ham"] = "/user/alexeyak/Programming/libra-gamess_interface/run/sd_ham/" # directory where SD basis vibronic hamiltonians will be printed out
+params["res"] = "/user/alexeyak/Programming/libra-gamess_interface/run/res/"
+params["mo_ham"] = "/user/alexeyak/Programming/libra-gamess_interface/run/mo_ham/" # directory where MO basis vibronic hamiltonians will be printed out
+params["sd_ham"] = "/user/alexeyak/Programming/libra-gamess_interface/run/sd_ham/" # directory where SD basis vibronic hamiltonians will be printed out
 
 
 params["traj_file"] = params["res"]+"md.xyz"
 params["ene_file"] = params["res"]+"ene.dat"
+params["mu_file"] = params["res"]+"mu.dat"
 params["se_pop_prefix"] = "out/"  # where the results of the TD-SE calculations will be printed out 
 
 params["print_coherences"] = 1 # a flag to compute and print electronic coherences (c^*_i * c_j) : option 0 -> no , 1 -> yes
+params["debug_mu_output"] = 1 # print the debug info into standard output: transition dipole moment matrices
+params["debug_densmat_output"] = 0 # print the debug info into standard output: density matrices, also including for the wavefunctions at different time steps
+params["print_mo_ham"] = 1 # print full and reduced size MO basis vibronic Hamiltonian
 
 # ***************************************************************
 # Excited electronic states
