ab49c037237ad3d2b8f13b553c70e7e70515882a
kosukesato <e1667@mosk.tytlabs.co.jp>
2016-12-06 15:59:01 +0900

    linked original system object to UFF one, and deleted syst_mm

--- src/include_mm.py
+++ src/include_mm.py
@@ -10,7 +10,7 @@
 #*********************************************************************************/
 
 ## \file include_mm.py
-# This module defines functions initializing "System" and "Hamiltonian" objects for MM part.
+# This module defines a function initializing "Hamiltonian" objects for MM part.
 
 import os
 import sys
@@ -23,56 +23,23 @@ elif sys.platform=="linux" or sys.platform=="linux2":
     from liblibra_core import *
 from libra_py import *
 
-def init_system(mb_functional, R_vdw_on, R_vdw_off, elem_file, uff_file, ent_file):
-    #\param[in]       mb_functional vdw functional type
-    #\param[in] R_vdw_on, R_vdw_off vdw interaction is included in the range of R_vdw_off and R_vdw_on  
-    #\param[in]           elem_file containing elements information
-    #\param[in]            uff_file containing UFF parameters
-    #\param[in]            ent_file containing molecular information
+def init_hamiltonian_mm(syst, uff):
+    #\param[in,out]     syst System object including atomic coordinates and connectivity information
+    #\param[in]          uff Forcefield object indicating Universal Force Field
     #
-    # returned variable -> syst_mm System object for MM part
+    # returned variable -> ham_mm  MM hamiltonian linked to syst
 
-    syst_mm = System()
-
-    # Create Universe and populate it
-    U = Universe(); LoadPT.Load_PT(U, elem_file)
-    # Create force field
-    uff = ForceField({"mb_functional":mb_functional,"R_vdw_on":R_vdw_on,"R_vdw_off":R_vdw_off })
-    LoadUFF.Load_UFF(uff, uff_file)
-
-    LoadMolecule.Load_Molecule(U, syst_mm, ent_file,"pdb")
-
-    return syst_mm
-
-def init_hamiltonian_ele(syst_mm, mb_functional, R_vdw_on, R_vdw_off, elem_file, uff_file):
-    #\param[in,out]         syst_mm system object for MM part.
-    #\param[in]       mb_functional vdw functional type
-    #\param[in] R_vdw_on, R_vdw_off vdw interaction is included in the range of R_vdw_off R_vdw_off and R_vdw_on
-    #\param[in]           elem_file containing elements information
-    #\param[in]            uff_file containing UFF parameters 
-    #
-    # returned variable -> ham_mm  MM hamiltonian linked to syst_mm
-    #                       el_mm  list of electronic(1,0)
     ham_mm = []
-    el_mm = []
 
-    for i in xrange(len(syst_mm)):
+    for i in xrange(len(syst)):
 
-        # Create Universe and populate it                                                                                                                    
-        U = Universe(); LoadPT.Load_PT(U, elem_file)
-        # Create force field                                                                                                                                 
-        uff = ForceField({"mb_functional":mb_functional,"R_vdw_on":R_vdw_on,"R_vdw_off":R_vdw_off })
-        LoadUFF.Load_UFF(uff, uff_file)
-
-        ham = Hamiltonian_Atomistic(1, 3*syst_mm[i].Number_of_atoms)
+        ham = Hamiltonian_Atomistic(1, 3*syst[i].Number_of_atoms)
         ham.set_Hamiltonian_type("MM")
-        atlst1=range(1,syst_mm[i].Number_of_atoms+1)
-        ham.set_interactions_for_atoms(syst_mm[i], atlst1, atlst1, uff, 1, 0) # 0 - verb, 0 - assign_rings
-        ham.set_system(syst_mm[i])
+        atlst1 = range(1,syst[i].Number_of_atoms+1)
+        ham.set_interactions_for_atoms(syst[i], atlst1, atlst1, uff, 1, 0) # 0 - verb, 0 - assign_rings
+        ham.set_system(syst[i])
         ham.show_interactions_statistics()
         ham.compute()
         ham_mm.append(ham)
 
-        el_mm.append(Electronic(1,0))
-
-    return ham_mm, el_mm
+    return ham_mm
