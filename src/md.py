e2e123d0ea4ca2b84fa259a8adb775ea5dc705b7
kosukesato <e1667@mosk.tytlabs.co.jp>
2017-03-14 09:31:32 +0900

    compute all kinds of energies at the same time (t+dt)

--- src/md.py
+++ src/md.py
@@ -325,11 +325,12 @@ def run_MD(syst,el,ao,E,sd_basis,params,label,Q, active_space):
                         # according to new convention (yet to be implemented for GMS and need to
                         # check for QE - the Hamiltonians will contain the total energies of 
                         # excited states, so no need for reference energy)
-                        epot[cnt] = compute_forces(mol[cnt], el[cnt], ham[cnt], f_pot)  #  f_pot = 0 - Ehrenfest, 1 - TSH
-                        epot[cnt] = qm_frac*epot[cnt] + mm_frac*epot_mm[cnt]
-                        ekin[cnt] = compute_kinetic_energy(mol[cnt])
-                        etot[cnt] = epot[cnt] + ekin[cnt]
-                        eext[cnt] = etot[cnt]
+                        epot[cnt] = compute_forces(mol[cnt], el[cnt], ham[cnt], f_pot)  # f_pot = 0 - Ehrenfest, 1 - TSH
+                        epot[cnt] = E[cnt].get(el[cnt].istate,el[cnt].istate)           # evaluated @ t+dt
+                        epot[cnt] = qm_frac*epot[cnt] + mm_frac*epot_mm[cnt]            # function of q(t+dt)
+                        ekin[cnt] = compute_kinetic_energy(mol[cnt])                    # function of p(t+dt/2)
+                        #etot[cnt] = epot[cnt] + ekin[cnt]
+                        #eext[cnt] = etot[cnt]
 
                         t.stop()
                         print "time after computing epot and ekin, eext, etot=",t.show(),"sec"
@@ -341,11 +342,20 @@ def run_MD(syst,el,ao,E,sd_basis,params,label,Q, active_space):
                         mol[cnt].propagate_p(0.5*dt_nucl) # p(t + dt/2) -> p(t + dt)
 
                         # optional thermostat
+                        ebat = 0.0
                         if MD_type == 1 and params["Ncool"] < i: # NVT-MD
                             for k in xrange(3*syst[cnt].Number_of_atoms):
                                 mol[cnt].p[k] = mol[cnt].p[k] * therm[cnt].vel_scale(0.5*dt_nucl)
 
-                            eext[cnt] = eext[cnt] + therm[cnt].energy() 
+                            #eext[cnt] = eext[cnt] + therm[cnt].energy() 
+                            ebat = therm[cnt].energy() 
+
+                        #epot[cnt] = compute_forces(mol[cnt], el[cnt], ham[cnt], f_pot)  #  f_pot = 0 - Ehrenfest, 1 - TSH 
+                        #epot[cnt] = E[cnt].get(0,0) 
+                        #epot[cnt] = qm_frac*epot[cnt] + mm_frac*epot_mm[cnt]
+                        ekin[cnt] = compute_kinetic_energy(mol[cnt]) # p(t+dt)
+                        etot[cnt] = epot[cnt] + ekin[cnt]            # q(t+dt) & p(t+dt)
+                        eext[cnt] = etot[cnt] + ebat                 # q(t+dt) & p(t+dt) & therm(t+dt)
 
                         # >>>>>>>>>>> Nuclear propagation ends <<<<<<<<<<<<
                         
