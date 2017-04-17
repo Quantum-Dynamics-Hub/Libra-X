4e0c448df8effee37a8c9648afcb29cb72c26487
Ekadashi Pradhan <ekadashi@srv-k07-14.cbls.ccr.buffalo.edu>
2017-04-17 12:08:27 -0400

    electronic smearing parameter passed externally

--- src/main.py
+++ src/main.py
@@ -178,11 +178,11 @@ def main(params):
                 if params["nspin"] == 2:
                     en_alp = qe_extract_eigenvalues("x%i.save/K00001/eigenval1.xml"%ex_st,nel)
                     en_bet = qe_extract_eigenvalues("x%i.save/K00001/eigenval2.xml"%ex_st,nel)
-                    occ_alp = fermi_pop(en_alp,nel,params["nspin"],params["el_sm"])
-                    occ_bet = fermi_pop(en_bet,nel,params["nspin"],params["el_sm"])
+                    occ_alp = fermi_pop(en_alp,nel,params["nspin"],params["electronic_smearing"])
+                    occ_bet = fermi_pop(en_bet,nel,params["nspin"],params["electronic_smearing"])
                 if params["nspin"] == 1:
                     en_orb = qe_extract_eigenvalues("x%i.save/K00001/eigenval.xml"%ex_st,nel)
-                    occ = fermi_pop(en_orb,nel,params["nspin"],params["el_sm"])
+                    occ = fermi_pop(en_orb,nel,params["nspin"],params["electronic_smearing"])
 
 
     ###########################################
