2851c72a7c26d2daaab51b94142f31453d86a7fc
Alexey Akimov <alexvakimov@gmail.com>
2017-04-19 11:15:34 -0400

    Merged development into master and added some changes in the variables naming

--- run/run_gms.py
+++ run/run_gms.py
@@ -12,7 +12,7 @@ elif sys.platform=="linux" or sys.platform=="linux2":
     from liblibra_core import *
 from libra_py import *
 
-user = 1 # 0 for Alexey, 1 for Kosuke, and 2 for Ekadashi; others should input the path they use
+user = 0 # 0 for Alexey, 1 for Kosuke, and 2 for Ekadashi; others should input the path they use
 test = 0 # 0 for 1 water molecule; 1 for 23 water molecules
 
 # input the paths of libra binary files and libra-gamess_interface source files. 
@@ -23,18 +23,19 @@ libra_gamess_int_path = "" # set the path name to the source files in libra-game
 if user==0:
     # For Alexey
     libra_bin_path = "/projects/academic/alexeyak/alexeyak/libra-dev/libracode-code/_build/src" 
-    libra_gamess_int_path = "/user/alexeyak/Programming/libra-gamess_interface/src" 
+    libra_x_path = "/user/alexeyak/Programming/Libra-X/src" 
+
 elif user==1:
     # For Kosuke
     libra_bin_path = "/home/e1667/install/libra-code/_build/src"
-    libra_gamess_int_path = "/home/e1667/dev/libra-gamess_interface/src"
+    libra_x_path = "/home/e1667/dev/libra-gamess_interface/src"
 
 elif user==2:
     # For Ekadashi
     libra_bin_path = "/projects/academic/alexeyak/ekadashi/libra-dev/libracode-code/_build/src"
-    libra_gamess_int_path = "/projects/academic/alexeyak/ekadashi/devel/libra-gamess_interface/src"
+    libra_x_path = "/projects/academic/alexeyak/ekadashi/devel/libra-gamess_interface/src"
 
-os.environ["src_path"] = libra_gamess_int_path
+os.environ["src_path"] = libra_x_path
 sys.path.insert(1,os.environ["src_path"]) # Path to the source code
 
 ########## Setup all manual parameters here ####################
@@ -139,9 +140,10 @@ params["sd_ham"] = ""  # directory where SD basis vibronic hamiltonians will be 
 
 if user==0:
     # For Alexey
-    params["res"] = "/user/alexeyak/Programming/libra-gamess_interface/run/res/"
-    params["mo_ham"] = "/user/alexeyak/Programming/libra-gamess_interface/run/mo_ham/" 
-    params["sd_ham"] = "/user/alexeyak/Programming/libra-gamess_interface/run/sd_ham/" 
+    params["res"] = "/user/alexeyak/Programming/Libra-X/run/res/"
+    params["mo_ham"] = "/user/alexeyak/Programming/Libra-X/run/mo_ham/" 
+    params["sd_ham"] = "/user/alexeyak/Programming/Libra-X/run/sd_ham/" 
+
 elif user==1:
     # For Kosuke
     cwd = os.getcwd()
@@ -175,6 +177,8 @@ params["check_tsh_probabilities"] = 0      # print the hopping probabilities if 
 from states import *
 
 # create excitation list
+# IMPORTANT: This should be consistent with the min_shift and max_shift parameters^M
+# that define the active space^M
 params["excitations"] = [ excitation(0,1,0,1), excitation(0,1,1,1), excitation(-1,1,1,1) ] 
 #params["excitations"] = [ excitation(0,1,0,1)]
 params["excitations_init"] = [0]
@@ -198,3 +202,5 @@ import main        # import main module of the libra-Gamess-interface code
 
 #data, test_data = main.main(params)  # run actual calculations
 main.main(params)  # run actual calculations
+
+
