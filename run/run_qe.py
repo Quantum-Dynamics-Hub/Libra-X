b87279b07072088da3dc7f274d6e27b6a084ebea
Ekadashi Pradhan <ekadashi@srv-k07-14.cbls.ccr.buffalo.edu>
2017-04-21 12:38:52 -0400

    minor indentation correction

--- run/run_qe.py
+++ run/run_qe.py
@@ -50,7 +50,7 @@ params["el_mts"] = 1
 params["num_SH_traj"] = 1
 
 params["non-orth"] = 1  # = 1 when MOs are non-orthogonal, = 0 when calculated in orthogonal MO basis
-params["print_S_mat"] = 1 # 1 if S-matrix printing required, 0 if not required
+params["print_S_mat"] = 0 # 1 if S-matrix printing required, 0 if not required
 params["smat_inc"] = 0 # 1 Including overlap matrix (S), 0 when overlap matrix (S) not included in el propagation
 
 params["MD_type"] = 0  # 1 NVT ensamble, 0 NVE ensamble
